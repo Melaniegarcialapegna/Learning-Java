a151950d09a0e9a7730421f0556887f68a9147fa
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2025-01-02 15:10:56 -0300

    UpLoad

---
+++ Introduccion/HolaMundo.java
@@ -0,0 +1,8 @@
+//Class -> nombre del archivo
+//Lineas 3,4 estructura basica
+class HolaMundo {
+    public static void main(String[] args){ //Metodo principal
+        //Instruccion para que el mensaje se vea en consola
+        System.out.println("Hola Mundo!");//Para trabajar con texto -> comillas dobles
+    }
+}
