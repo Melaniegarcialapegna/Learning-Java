238c83c45ac92b58670d848c7cfc92215aab665e
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2025-01-02 15:09:24 -0300

    Avances

---
+++ TiposDeDatos/1DeclaracionVariables.java
@@ -0,0 +1,21 @@
+package main;
+public class Variables {
+    public static void main(String[] args){
+        /*
+        * 4 tipos de datos
+        *
+        *Enteros
+        *Decimales
+        *Caracteres
+        *Booleanos
+        * */
+
+        int version; //Declaro variable -> reservo espacio en memoria
+        version = 4;
+        System.out.println(version);
+
+        //Inicializarla con un valor
+        int version2 = 4;
+        System.out.println(version2);
+    }
+}
---
+++ TiposDeDatos/2TiposDatos.java
@@ -0,0 +1,38 @@
+/*
+* Java es un lenguaje FUERTEMENTE TIPADO
+* Durante todo el programa esa variable va a ser de ese tipo de dato
+*
+* GRANDES GRUPOS
+* Enteros
+* Flotantes -> deben terminar con sufijo "f"
+* Caracteres
+* Booleanos
+*
+* TIPOS DE DATOS -> Tipos primitivos
+* byte (Enteros)
+* short (Enteros)
+* int (Enteros)
+* long (Enteros)
+* char (Caracteres
+* float (Flotantes)
+* double (Flotantes)
+* boolean (Booleanos)
+* */
+
+package main;
+public class Variables {
+    public static void main(String[] args){
+
+        int edad = 20;
+
+        float edadMes = 20.2f;
+
+        char nombre = ´Melanie´; //Comillas simples
+        char letraAMinuscula1 = ´a´;
+        char letraAMinuscula2 = 97 ;
+
+        boolean feliz = true;
+        boolean triste = false;
+
+    }
+}
